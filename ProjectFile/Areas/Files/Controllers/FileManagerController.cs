6277b21e742bbd14453e46e170cc5c2779bb763b
ToDinhTruc <101381736+Mayenc@users.noreply.github.com>
2023-07-21 09:53:17 +0700

    update commit

---
+++ ProjectFile/Areas/Files/Controllers/FileManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectFile.Areas.Files.Controllers
+{
+    [Area("Files")]
+    public class FileManagerController : Controller
+    {
+        [Route("/file-manager")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
